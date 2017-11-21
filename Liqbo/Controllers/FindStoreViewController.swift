01e9126b4646e45167cb64da838f53f20eafeb55
christopherho96 <ho.christopher.tak@gmail.com>
2017-11-21 00:25:26 -0500

    setup CoreLocation GPS grab for api call

--- Liqbo/Controllers/FindStoreViewController.swift
+++ Liqbo/Controllers/FindStoreViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
 
-class FindStoreViewController: UIViewController {
+class FindStoreViewController: UIViewController, CLLocationManagerDelegate {
 
+    
+    let ACCESS_KEY = "MDpmYjcyMDI5MC1jNjkwLTExZTctODFkNi01Nzk0MGZlMTcyMDE6a2ZTczF5ZXVnckEyMDgwZXBSeDVmZDNpYUVIYk5mTmo0azFC"
+    
+    var LCBO_SEARCH_ANY_STORE_URL = "https://lcboapi.com/stores?access_key=MDpmYjcyMDI5MC1jNjkwLTExZTctODFkNi01Nzk0MGZlMTcyMDE6a2ZTczF5ZXVnckEyMDgwZXBSeDVmZDNpYUVIYk5mTmo0azFC"
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+       
         
         //removes 1px line from bottom of navigation tab bar
         self.navigationController!.navigationBar.isTranslucent = false
@@ -25,15 +40,49 @@ class FindStoreViewController: UIViewController {
 
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getStoreData (url: String, parameters: [String: String]){
+        
+        //making http request with alamofire
+        
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON { (response) in
+            
+            if response.result.isSuccess{
+                
+                print("Success, got the store data")
+                
+                //the JSON() cast comes from SWiftyJSON library
+                let storesJSON : JSON = JSON(response.result.value!)
+                print(storesJSON["result"].count)
+                print(storesJSON["result"][0]["name"])
+                //self.updateSearchItemsData(json: storesJSON)
+                
+            }else{
+                print("Error \(response.result.error!)")
+            }
+        }
     }
-    */
+
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //access the last element of locations array which is usually the most accurate
+        let location = locations[locations.count - 1]
+        
+        if location.horizontalAccuracy > 0{
+            locationManager.stopUpdatingLocation()
+            
+            print("longitude: \(location.coordinate.longitude), latitude: \(location.coordinate.latitude)")
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params : [String : String] = ["lat": latitude, "lon" : longitude]
+            getStoreData(url: LCBO_SEARCH_ANY_STORE_URL, parameters: params)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+
 
 }
--- Liqbo/Models/StoreDataModel.swift
+++ Liqbo/Models/StoreDataModel.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class StoreDateModel{
+    
+    var address_line_1 = ""
+    var city = ""
+    var latitude: Float = 0
+    var longitude: Float = 0
+    
+    var sunday_open: Float = 0
+    var sunday_close: Float = 0
+    var monday_open: Float = 0
+    var monday_close: Float = 0
+    var tuesday_open: Float = 0
+    var tuesday_close: Float = 0
+    var wednesday_open: Float = 0
+    var wednesday_close: Float = 0
+    var thursday_open: Float = 0
+    var thursday_close: Float = 0
+    var friday_open: Float = 0
+    var friday_close: Float = 0
+    var saturday_open: Float = 0
+    var saturday_close: Float = 0
+    
+}
